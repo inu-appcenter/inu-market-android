793544f723c07cbc9f2536cb1c3757ead1e6a386
gowoonJ <jgw972229@gmail.com>
2022-02-24 17:59:19 +0900

    Chore: my page 뷰모델 추가 (진행 중)

--- app/src/main/java/com/example/inomtest/MyPageViewModel.kt
+++ app/src/main/java/com/example/inomtest/MyPageViewModel.kt
@@ -1,5 +1,20 @@
 package com.example.inomtest
 
-class MyPageViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyPageViewModel : ViewModel() {
+    val nickName = MutableLiveData("")
+    val temperature = MutableLiveData(0)
+
+    // load user profile
+    fun loadProfile() {
+
+    }
+
+    fun setProfile() {
+        nickName.postValue("")
+
+    }
 
 }
