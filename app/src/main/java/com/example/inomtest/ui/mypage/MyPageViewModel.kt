42423897c588c39f01be0c11dc9dc0c4109d6abb
gowoonJ <chocopie_fine@kyobobook.com>
2022-03-07 17:38:09 +0900

    Chore: 코루틴 세팅 코드 추가 중

--- app/src/main/java/com/example/inomtest/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/example/inomtest/ui/mypage/MyPageViewModel.kt
@@ -2,19 +2,15 @@ package com.example.inomtest.ui.mypage
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.inomtest.network.RetrofitManager
 
 class MyPageViewModel : ViewModel() {
     val nickName = MutableLiveData("이름")
     val temperature = MutableLiveData(0)
 
     // load user profile
-    fun loadProfile() {
-
-    }
-
-    fun setProfile() {
-        nickName.postValue("")
-
+    suspend fun loadProfile() {
+        val service = RetrofitManager
     }
 
 }
